3ffde43718e4cc7779e7bff9cd33db0c23c8427c
MehdiNe <mahdimahdi9731@gmail.com>
2024-07-12 03:21:01 +0330

    first commit

---
+++ src/features/auth/LoginForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LoginForm() {
+  return (
+    <div>
+      <div>
+        <h2>خوش اومدی</h2>
+        <p>برای ورود فرم زیر رو پر کن</p>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
