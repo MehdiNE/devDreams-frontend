93f44b2b8413502ebb24fb0ae5d09e4046b4a0cf
MehdiNe <mahdimahdi9731@gmail.com>
2024-07-12 16:46:01 +0330

    progress on login

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -1,14 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 function LoginForm() {
   return (
-    <div>
-      <div>
+    <div className="p-10">
+      <div className="mb-10 font-medium">
         <h2>خوش اومدی</h2>
-        <p>برای ورود فرم زیر رو پر کن</p>
+        <p className="text-sm">برای ورود فرم زیر رو پر کن</p>
       </div>
-      <Button>سلام</Button>
+
+      <div className="space-y-4">
+        <Input />
+        <Input />
+        <Button>ورود</Button>
+      </div>
+
+      <div className="flex"></div>
     </div>
   );
 }
